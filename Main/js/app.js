6ed56a7ee48305fa3a25bcb750db583e37a5f2d7
Łukasz Kowalik <lukaszkowalik25@gmail.com>
2020-04-09 17:55:02 +0200

    dodłem funkcję wyboru ćwiczeń

--- Main/js/app.js
+++ Main/js/app.js
@@ -1,14 +1,87 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Exercise from "./Components/exercise";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
 
 import "../scss/style.scss";
+import exercise from "./Components/exercise";
 
-class App extends Component {
+class Ex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: [],
+      name: "",
+    };
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log("działa button");
+    this.setState({
+      name: "",
+      id: [...this.state.id, this.state.name],
+      add: "",
+    });
+  };
+
+  onChange = (event) => {
+    this.setState({ add: event.target.value });
+    console.log(add);
+  };
+
   render() {
-    return <div>Ćwicz cięzko </div>;
+    console.log("option?" + this.state.add);
+    return (
+      <div>
+        <Form onSubmit={this.onSubmit} onChange={this.onSubmit}>
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Wszystkie ćwiczenia</Form.Label>
+            <Form.Control as="select" custom>
+              {this.props.Exercise.map((el) => (
+                <option
+                  value={this.state.name}
+                  key={el.id}
+                  onChange={this.onSubmit}
+                >
+                  {el.name}
+                </option>
+              ))}
+            </Form.Control>
+            <Button
+              value={this.state.name}
+              variant="dark"
+              type="submit"
+              // onChange={this.handleSubmit}
+            >
+              Wybierz
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      add: "",
+    };
+  }
+
+  render() {
+    console.log("sprawdzam czy działa" + this.state.add);
+    return (
+      <div>
+        <Ex Exercise={exercise} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App Exercise={Exercise} />, document.getElementById("app"));
